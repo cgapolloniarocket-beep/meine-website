32b321969531ecbc2dbba0422bddb603ea1254da
Apollonia Space <cgapolloniarocket@gmail.com>
2026-06-14 12:22:48 +0200

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.apolloniaspace.com',
+  output: 'hybrid',
+  adapter: cloudflare(),
+ // @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.apolloniaspace.com',
+  output: 'hybrid',
+  adapter: cloudflare(),
   integrations: [sitemap()],
 });
